88de558987d27812a21f60d2fbc482cb92db8fb0
Alex666 <08evalus07@gmail.com>
2019-12-04 21:13:33 +0200

    OD19DEVAT4-31 Changed type Integer to BigIntegers for ID. Added check for NULL. Added new test for check IllegalArguments. Updated code style.
    Signed-off-by: Alex666 <08evalus07@gmail.com>

--- src/main/java/com/netcracker/testerritto/mappers/RemarkRowMapper.java
+++ src/main/java/com/netcracker/testerritto/mappers/RemarkRowMapper.java
@@ -3,6 +3,7 @@ package com.netcracker.testerritto.mappers;
 import com.netcracker.testerritto.models.Remark;
 import org.springframework.jdbc.core.RowMapper;
 
+import java.math.BigInteger;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
@@ -11,10 +12,10 @@ public class RemarkRowMapper implements RowMapper<Remark> {
     @Override
     public Remark mapRow(ResultSet resultSet, int i) throws SQLException {
         Remark remark = new Remark();
-        remark.setId(resultSet.getInt("id"));
+        remark.setId(new BigInteger(resultSet.getString("id")));
         remark.setText(resultSet.getString("text"));
-        remark.setUserId(resultSet.getInt("userId"));
-        remark.setQuestionId(resultSet.getInt("questionId"));
+        remark.setUserSenderId(new BigInteger(resultSet.getString("userId")));
+        remark.setQuestionId(new BigInteger(resultSet.getString("questionId")));
         return remark;
     }
 }
--- src/test/java/com/netcracker/testerritto/dao/TestRemarkDAO.java
+++ src/test/java/com/netcracker/testerritto/dao/TestRemarkDAO.java
@@ -3,25 +3,18 @@ package com.netcracker.testerritto.dao;
 import com.netcracker.testerritto.ApplicationConfiguration;
 import com.netcracker.testerritto.DataSourceConfig;
 import com.netcracker.testerritto.models.Remark;
-import com.netcracker.testerritto.services.RemarkService;
 import org.junit.After;
-import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.datasource.DataSourceTransactionManager;
-import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
-import org.springframework.transaction.PlatformTransactionManager;
 
-import javax.sql.DataSource;
-
+import java.math.BigInteger;
 import java.util.Locale;
 
 import static org.junit.Assert.assertEquals;
@@ -37,80 +30,110 @@ public class TestRemarkDAO {
     @Autowired
     private RemarkDAO remarkDAO;
 
-    private Integer authorId;
-    private Integer groupId;
-    private Integer testId;
-    private Integer questionId;
-    private Integer remarkerId;
-    private Integer sequenceId;
+    private BigInteger authorId;
+    private BigInteger groupId;
+    private BigInteger testId;
+    private BigInteger questionId;
+    private BigInteger remarkerId;
+    private BigInteger sequenceId;
+    private static String CURRENT_SEQUENCE = "select object_id_pr.currval from dual";
 
     @Before
     public void setup(){
         Locale.setDefault(Locale.ENGLISH);
+        String sql =
+                        "insert all\n" +
+                        "    into objects(object_id, parent_id, object_type_id, name, description)\n" +
+                        "        values(object_id_pr.nextval, ?, ?, ?, null)\n" +
+                        "select * from dual";
+
+        jdbcTemplate.update(sql,null, 1, "USER_AUTHOR "+"object_id_pr.currval");
+        authorId = jdbcTemplate.queryForObject(CURRENT_SEQUENCE, BigInteger.class);
+
+        jdbcTemplate.update(sql, null, 2, "GROUP " + "object_id_pr.currval");
+        groupId = jdbcTemplate.queryForObject(CURRENT_SEQUENCE, BigInteger.class);
+
         jdbcTemplate.update(
-                "INSERT ALL\n" +
-                        "  INTO objects(object_id, parent_id, object_type_id, name, description)\n" +
-                        "      VALUES(object_id_PR.NEXTVAL, null, 1, 'USER_AUTHOR '|| object_id_PR.CURRVAL, null)\n" +
-                        "SELECT * FROM DUAL");
-        authorId = jdbcTemplate.queryForObject("SELECT object_id_pr.CURRVAL FROM DUAL", Integer.class);
-        jdbcTemplate.update(
-                "INSERT ALL\n" +
-                        "    INTO objects(object_id, parent_id, object_type_id, name, description)\n" +
-                        "\t\tVALUES(object_id_PR.NEXTVAL, null, 2, 'GROUP '|| object_id_PR.CURRVAL, null)\n" +
-                        "SELECT * FROM DUAL");
-        groupId = jdbcTemplate.queryForObject("SELECT object_id_pr.CURRVAL FROM DUAL", Integer.class);
-        jdbcTemplate.update(
-                "INSERT ALL        \n" +
-                        "\tINTO objects(object_id, parent_id, object_type_id, name, description)\n" +
-                        "        VALUES(object_id_PR.NEXTVAL, object_id_PR.CURRVAL-1, 4, 'TEST '|| object_id_PR.CURRVAL, null)\t \n" +
-                        "\tINTO objreference(attr_id, object_id, reference) /* CREATE_TEST_BY */\n" +
-                        "        VALUES(24, object_id_PR.CURRVAL, object_id_PR.CURRVAL-1) \n" +
-                        "SELECT * FROM DUAL");
-        testId = jdbcTemplate.queryForObject("SELECT object_id_pr.CURRVAL FROM DUAL", Integer.class);
-        jdbcTemplate.update(
-                "INSERT ALL     \n" +
-                        "\tINTO objects(object_id, parent_id, object_type_id, name, description)\n" +
-                        "\t\tVALUES(object_id_PR.NEXTVAL, object_id_PR.CURRVAL-1, 10, 'QUESTION '|| object_id_PR.CURRVAL, null)\n" +
-                        "SELECT * FROM DUAL");
-        questionId = jdbcTemplate.queryForObject("SELECT object_id_pr.CURRVAL FROM DUAL", Integer.class);
-        jdbcTemplate.update(
-                "INSERT ALL     \n" +
-                        "\tINTO objects(object_id, parent_id, object_type_id, name, description)\n" +
-                        "\t\tVALUES(object_id_PR.NEXTVAL, null, 1, 'USER_REMARKER '|| object_id_PR.CURRVAL, null)\n" +
-                        "SELECT * FROM DUAL");
-        remarkerId = jdbcTemplate.queryForObject("SELECT object_id_pr.CURRVAL FROM DUAL", Integer.class);
-        remarkDAO.createRemark(remarkerId, questionId,"New Remark Text");
-        sequenceId = jdbcTemplate.queryForObject("SELECT object_id_pr.CURRVAL FROM DUAL", Integer.class);
+                    "insert all\n" +
+                        "    into objects(object_id, parent_id, object_type_id, name, description)\n" +
+                        "        values(object_id_pr.nextval, ?, 4, 'TEST '|| object_id_pr.currval, null)\n" +
+                        "    into objreference(attr_id, object_id, reference) /* CREATE_TEST_BY */\n" +
+                        "        values(24, object_id_pr.currval, ?)\n" +
+                        "select * from dual", groupId.toString(), groupId.toString());
+        testId = jdbcTemplate.queryForObject(CURRENT_SEQUENCE, BigInteger.class);
+
+        jdbcTemplate.update(sql, testId.toString(), 10, "QUESTION " + "object_id_pr.currval");
+        questionId = jdbcTemplate.queryForObject(CURRENT_SEQUENCE, BigInteger.class);
+
+        jdbcTemplate.update(sql, null, 1, "USER_REMARKER " + "object_id_pr.currval");
+        remarkerId = jdbcTemplate.queryForObject(CURRENT_SEQUENCE, BigInteger.class);
     }
 
     @After
     public void setDown(){
-        jdbcTemplate.update("DELETE FROM objects WHERE object_id = ?", authorId);
-        jdbcTemplate.update("DELETE FROM objects WHERE object_id = ?", groupId);
-        jdbcTemplate.update("DELETE FROM objects WHERE object_id = ?", testId);
-        jdbcTemplate.update("DELETE FROM objects WHERE object_id = ?", questionId);
-        jdbcTemplate.update("DELETE FROM objects WHERE object_id = ?", remarkerId);
-        jdbcTemplate.update("DELETE FROM objects WHERE object_id = ?", sequenceId);
+        String sql =
+               "delete from\n" +
+               "    objects\n" +
+               "where\n" +
+               "    object_id = ?";
+        jdbcTemplate.update(sql, authorId.toString());
+        jdbcTemplate.update(sql, groupId.toString());
+        jdbcTemplate.update(sql, testId.toString());
+        jdbcTemplate.update(sql, questionId.toString());
+        jdbcTemplate.update(sql, remarkerId.toString());
+        if(sequenceId != null)
+            jdbcTemplate.update(sql, sequenceId.toString());
     }
 
     @Test
-    public void insertGetRemark() throws Exception{
+    public void insertGetRemarkById() throws Exception{
+        remarkDAO.createRemark(remarkerId, questionId,"New Remark Text");
+        sequenceId = jdbcTemplate.queryForObject(CURRENT_SEQUENCE, BigInteger.class);
+
         Remark remark = remarkDAO.getRemarkById(sequenceId);
 
         Remark remarkExpected = new Remark();
         remarkExpected.setText("New Remark Text");
         remarkExpected.setQuestionId(questionId);
-        remarkExpected.setUserId(remarkerId);
+        remarkExpected.setUserSenderId(remarkerId);
         remarkExpected.setId(sequenceId);
 
         assertEquals(remarkExpected, remark);
     }
 
+    @Test(expected = IllegalArgumentException.class)
+    public void getRemarkByIdNull() throws Exception{
+        Remark remark = remarkDAO.getRemarkById(null);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void insertRemarkUserIdNull() throws Exception{
+        remarkDAO.createRemark(null, questionId, "New Remark Text");
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void insertRemarkQuestionIdNull() throws Exception{
+        remarkDAO.createRemark(authorId, null, "New Remark Text");
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void insertRemarkRemarkTextNull() throws Exception{
+        remarkDAO.createRemark(authorId, questionId, "");
+    }
+
     @Test(expected = EmptyResultDataAccessException.class)
     public void deleteRemarkById() throws Exception{
+        remarkDAO.createRemark(remarkerId, questionId,"New Remark Text");
+        sequenceId = jdbcTemplate.queryForObject(CURRENT_SEQUENCE, BigInteger.class);
+
         remarkDAO.deleteRemark(sequenceId);
+
         Remark remark = remarkDAO.getRemarkById(sequenceId);
     }
 
+    @Test(expected = IllegalArgumentException.class)
+    public void deleteRemarkByIdNull() throws Exception{
+        remarkDAO.deleteRemark(null);
+    }
 
 }
