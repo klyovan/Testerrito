cbffa8433514a66a56a1262b9e39bc878578a917
AnastasiaRevutskaiia <anastasiarp2000@gmail.com>
2019-12-11 16:45:22 +0200

    Update CategoryDAO and GradeCategoryDAO selects query style

--- src/main/java/com/netcracker/testerritto/dao/CategoryDAO.java
+++ src/main/java/com/netcracker/testerritto/dao/CategoryDAO.java
@@ -4,25 +4,23 @@ import com.netcracker.testerritto.mappers.CategoryRowMapper;
 import com.netcracker.testerritto.models.Category;
 import com.netcracker.testerritto.properties.AttrtypeProperties;
 import com.netcracker.testerritto.properties.ObjtypeProperties;
-import java.math.BigInteger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.math.BigInteger;
+
 @Repository
 @Transactional
 public class CategoryDAO {
   @Autowired
   private JdbcTemplate jdbcTemplate;
-
   @Autowired
   private CategoryRowMapper categoryRowMapper;
-
-  public Category getCategoryById(BigInteger id) throws DataAccessException {
-    String getCategoryByIdQuery =
-      "select " +
+  private String getCategoryByIdQuery =
+    "select " +
       "  category.object_id id, " +
       "  category_attr.value name_category " +
       "from " +
@@ -30,17 +28,19 @@ public class CategoryDAO {
       "where " +
       "  category.object_id = ? " +
       "  and category.object_id = category_attr.object_id " +
-      "  and category_attr.attr_id = " + AttrtypeProperties.NAME_CATEGORY;
+      "  and category_attr.attr_id = 14 /* name_category */";
+
+  public Category getCategoryById(BigInteger id) throws DataAccessException {
     return jdbcTemplate.queryForObject(getCategoryByIdQuery, new Object[]{id.toString()}, categoryRowMapper);
   }
 
-  public void deleteCategoryById(BigInteger id) throws DataAccessException  {
+  public void deleteCategoryById(BigInteger id) throws DataAccessException {
     new ObjectEavBuilder.Builder(jdbcTemplate)
       .setObjectId(id)
       .delete();
   }
 
-  public Category updateCategory(Category category) throws DataAccessException  {
+  public Category updateCategory(Category category) throws DataAccessException {
     new ObjectEavBuilder.Builder(jdbcTemplate)
       .setObjectId(category.getId())
       .setStringAttribute(AttrtypeProperties.NAME_CATEGORY, category.getNameCategory())
@@ -48,7 +48,7 @@ public class CategoryDAO {
     return getCategoryById(category.getId());
   }
 
-  public BigInteger createCategory(Category newCategory) throws DataAccessException  {
+  public BigInteger createCategory(Category newCategory) throws DataAccessException {
     return new ObjectEavBuilder.Builder(jdbcTemplate)
       .setName(newCategory.getNameCategory())
       .setObjectTypeId(ObjtypeProperties.CATEGORY)
--- src/main/java/com/netcracker/testerritto/dao/GradeCategoryDAO.java
+++ src/main/java/com/netcracker/testerritto/dao/GradeCategoryDAO.java
@@ -4,25 +4,23 @@ import com.netcracker.testerritto.mappers.GradeCategoryMapper;
 import com.netcracker.testerritto.models.GradeCategory;
 import com.netcracker.testerritto.properties.AttrtypeProperties;
 import com.netcracker.testerritto.properties.ObjtypeProperties;
-import java.math.BigInteger;
-import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.math.BigInteger;
+import java.util.List;
 
 @Repository
 @Transactional
 public class GradeCategoryDAO {
   @Autowired
   private JdbcTemplate jdbcTemplate;
-
   @Autowired
   private GradeCategoryMapper gradeCategoryMapper;
-
-  public List<GradeCategory> getGradeCategoryByTestId(BigInteger id) {
-    String getGradeCategoryByTestIdQuery =
-      "select " +
+  private String getGradeCategoryByTestIdQuery =
+    "select " +
       "  grade_category.object_id id, " +
       "  grade_category.name grade_name, " +
       "  grade_category.parent_id test_id, " +
@@ -38,75 +36,82 @@ public class GradeCategoryDAO {
       "  objreference grade_category_ref_on_category " +
       "where " +
       "  grade_category.parent_id = ? " +
-      "  and grade_category.object_type_id = " + ObjtypeProperties.GRADE_CATEGORY +
-      "  and grade_category_min_value.attr_id = " + AttrtypeProperties.MIN_SCORE +
+      "  and grade_category.object_type_id = 9 /* grade_category */ " +
+      "  and grade_category_min_value.attr_id = 15 /* min_score */ " +
       "  and grade_category_min_value.object_id = grade_category.object_id " +
-      "  and grade_category_max_value.attr_id = " + AttrtypeProperties.MAX_SCORE +
+      "  and grade_category_max_value.attr_id = 16 /* max_score */ " +
       "  and grade_category_max_value.object_id = grade_category.object_id  " +
-      "  and grade_category_meaning.attr_id = " + AttrtypeProperties.MEANING +
+      "  and grade_category_meaning.attr_id = 17 /* meaning */ " +
       "  and grade_category_meaning.object_id = grade_category.object_id " +
       "  and grade_category_ref_on_category.object_id = grade_category.object_id " +
-      "  and grade_category_ref_on_category.attr_id = " + AttrtypeProperties.GRADE_BELONGS;
-    return jdbcTemplate.query(getGradeCategoryByTestIdQuery, new Object[]{id.toString()}, gradeCategoryMapper);
+      "  and grade_category_ref_on_category.attr_id = 33/* grade_belongs */";
+
+  public List<GradeCategory> getGradeCategoryByTestId(BigInteger id) {
+
+    return jdbcTemplate
+      .query(getGradeCategoryByTestIdQuery, new Object[]{id.toString()}, gradeCategoryMapper);
   }
 
   public List<GradeCategory> getGradeCategoryByCategoryId(BigInteger id) {
     String getGradeCategoryByCategoryIdQuery =
       "select " +
-      "  grade_category.object_id id, " +
-      "  grade_category.name grade_name, " +
-      "  grade_category.parent_id test_id, " +
-      "  grade_category_min_value.value min_value, " +
-      "  grade_category_max_value.value max_value, " +
-      "  grade_category_meaning.value meaning, " +
-      "  grade_category_ref_on_category.reference category_id " +
-      "from " +
-      "  objects grade_category, " +
-      "  attributes grade_category_min_value, " +
-      "  attributes grade_category_max_value, " +
-      "  attributes grade_category_meaning, " +
-      "  objreference grade_category_ref_on_category " +
-      "where " +
-      "  grade_category_ref_on_category.reference = ? " +
-      "  and grade_category_ref_on_category.attr_id = " + AttrtypeProperties.GRADE_BELONGS +
-      "  and grade_category_ref_on_category.object_id = grade_category.object_id " +
-      "  and grade_category.object_type_id = " + ObjtypeProperties.GRADE_CATEGORY +
-      "  and grade_category_min_value.attr_id = " + AttrtypeProperties.MIN_SCORE +
-      "  and grade_category_min_value.object_id = grade_category.object_id " +
-      "  and grade_category_max_value.attr_id = " + AttrtypeProperties.MAX_SCORE +
-      "  and grade_category_max_value.object_id = grade_category.object_id " +
-      "  and grade_category_meaning.attr_id = " + AttrtypeProperties.MEANING +
-      "  and grade_category_meaning.object_id = grade_category.object_id";
-    return jdbcTemplate.query(getGradeCategoryByCategoryIdQuery, new Object[]{id.toString()}, gradeCategoryMapper);
+        "  grade_category.object_id id, " +
+        "  grade_category.name grade_name, " +
+        "  grade_category.parent_id test_id, " +
+        "  grade_category_min_value.value min_value, " +
+        "  grade_category_max_value.value max_value, " +
+        "  grade_category_meaning.value meaning, " +
+        "  grade_category_ref_on_category.reference category_id " +
+        "from " +
+        "  objects grade_category, " +
+        "  attributes grade_category_min_value, " +
+        "  attributes grade_category_max_value, " +
+        "  attributes grade_category_meaning, " +
+        "  objreference grade_category_ref_on_category " +
+        "where " +
+        "  grade_category_ref_on_category.reference = ? " +
+        "  and grade_category_ref_on_category.attr_id = 33 /* grade_belongs */ " +
+        "  and grade_category_ref_on_category.object_id = grade_category.object_id " +
+        "  and grade_category.object_type_id = 9 /* grade_category */ " +
+        "  and grade_category_min_value.attr_id = 15 /* min_score */ " +
+        "  and grade_category_min_value.object_id = grade_category.object_id " +
+        "  and grade_category_max_value.attr_id = 16 /* max_score */ " +
+        "  and grade_category_max_value.object_id = grade_category.object_id " +
+        "  and grade_category_meaning.attr_id = 17 /* meaning */ " +
+        "  and grade_category_meaning.object_id = grade_category.object_id";
+    return jdbcTemplate
+      .query(getGradeCategoryByCategoryIdQuery, new Object[]{id.toString()}, gradeCategoryMapper);
   }
 
   public GradeCategory getGradeCategoryById(BigInteger id) {
     String getGradeCategoryByIdQuery =
       "select " +
-      "  grade_category.object_id id, " +
-      "  grade_category.parent_id test_id, " +
-      "  grade_category_min_value.value min_value, " +
-      "  grade_category_max_value.value max_value, " +
-      "  grade_category_meaning.value meaning, " +
-      "  grade_category_ref_on_category.reference category_id " +
-      "from " +
-      "  objects grade_category, " +
-      "  attributes grade_category_min_value, " +
-      "  attributes grade_category_max_value, " +
-      "  attributes grade_category_meaning, " +
-      "  objreference grade_category_ref_on_category " +
-      "where " +
-      "  grade_category.object_id = ? " +
-      "  and grade_category.object_type_id = " + ObjtypeProperties.GRADE_CATEGORY +
-      "  and grade_category_min_value.attr_id = " + AttrtypeProperties.MIN_SCORE +
-      "  and grade_category_min_value.object_id = grade_category.object_id  " +
-      "  and grade_category_max_value.attr_id = " + AttrtypeProperties.MAX_SCORE +
-      "  and grade_category_max_value.object_id = grade_category.object_id " +
-      "  and grade_category_meaning.attr_id = " + AttrtypeProperties.MEANING +
-      "  and grade_category_meaning.object_id = grade_category.object_id " +
-      "  and grade_category_ref_on_category.object_id = grade_category.object_id " +
-      "  and grade_category_ref_on_category.attr_id = " + AttrtypeProperties.GRADE_BELONGS;
-    return jdbcTemplate.queryForObject(getGradeCategoryByIdQuery, new Object[]{id.toString()}, gradeCategoryMapper);
+        "  grade_category.object_id id, " +
+        "  grade_category.parent_id test_id, " +
+        "  grade_category_min_value.value min_value, " +
+        "  grade_category_max_value.value max_value, " +
+        "  grade_category_meaning.value meaning, " +
+        "  grade_category_ref_on_category.reference category_id " +
+        "from " +
+        "  objects grade_category, " +
+        "  attributes grade_category_min_value, " +
+        "  attributes grade_category_max_value, " +
+        "  attributes grade_category_meaning, " +
+        "  objreference grade_category_ref_on_category " +
+        "where " +
+        "  grade_category.object_id = ? " +
+        "  and grade_category.object_type_id = 9 /* grade_category */ " +
+        "  and grade_category_min_value.attr_id = 15 /* min_score */ " +
+        "  and grade_category_min_value.object_id = grade_category.object_id " +
+        "  and grade_category_max_value.attr_id = 16 /* max_score */ " +
+        "  and grade_category_max_value.object_id = grade_category.object_id  " +
+        "  and grade_category_meaning.attr_id = 17 /* meaning */ " +
+        "  and grade_category_meaning.object_id = grade_category.object_id " +
+        "  and grade_category_ref_on_category.object_id = grade_category.object_id " +
+        "  and grade_category_ref_on_category.attr_id = 33/* grade_belongs */";
+
+    return jdbcTemplate.queryForObject(getGradeCategoryByIdQuery, new Object[]{id.toString()},
+      gradeCategoryMapper);
   }
 
   public BigInteger createGradeCategory(GradeCategory newGradeCategory) {
@@ -114,17 +119,21 @@ public class GradeCategoryDAO {
       .setName(newGradeCategory.getMeaning())
       .setObjectTypeId(ObjtypeProperties.GRADE_CATEGORY)
       .setParentId(newGradeCategory.getTestId())
-      .setStringAttribute(AttrtypeProperties.MIN_SCORE, String.valueOf(newGradeCategory.getMinScore()))
-      .setStringAttribute(AttrtypeProperties.MAX_SCORE, String.valueOf(newGradeCategory.getMaxScore()))
-      .setStringAttribute(AttrtypeProperties.MEANING, String.valueOf(newGradeCategory.getMeaning()))
-      .setReference(new BigInteger(String.valueOf(AttrtypeProperties.GRADE_BELONGS)), newGradeCategory.getCategoryId())
+      .setStringAttribute(AttrtypeProperties.MIN_SCORE,
+        String.valueOf(newGradeCategory.getMinScore()))
+      .setStringAttribute(AttrtypeProperties.MAX_SCORE,
+        String.valueOf(newGradeCategory.getMaxScore()))
+      .setStringAttribute(AttrtypeProperties.MEANING,
+        String.valueOf(newGradeCategory.getMeaning()))
+      .setReference(new BigInteger(String.valueOf(AttrtypeProperties.GRADE_BELONGS)),
+        newGradeCategory.getCategoryId())
       .create();
   }
 
   public void deleteGradeCategoryById(BigInteger id) {
     new ObjectEavBuilder.Builder(jdbcTemplate)
-        .setObjectId(id)
-        .delete();
+      .setObjectId(id)
+      .delete();
   }
 
   public void deleteGradeCategoryByTestId(BigInteger id) {
@@ -137,8 +146,10 @@ public class GradeCategoryDAO {
   public GradeCategory updateGradeCategory(GradeCategory gradeCategory) {
     new ObjectEavBuilder.Builder(jdbcTemplate)
       .setObjectId(gradeCategory.getId())
-      .setStringAttribute(AttrtypeProperties.MIN_SCORE, String.valueOf(gradeCategory.getMinScore()))
-      .setStringAttribute(AttrtypeProperties.MAX_SCORE, String.valueOf(gradeCategory.getMaxScore()))
+      .setStringAttribute(AttrtypeProperties.MIN_SCORE,
+        String.valueOf(gradeCategory.getMinScore()))
+      .setStringAttribute(AttrtypeProperties.MAX_SCORE,
+        String.valueOf(gradeCategory.getMaxScore()))
       .setStringAttribute(AttrtypeProperties.MEANING, gradeCategory.getMeaning())
       .update();
     return getGradeCategoryById(gradeCategory.getId());
