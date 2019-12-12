0c2402f477eb347932f5dc29e9c53a7e3a677782
Alex666 <08evalus07@gmail.com>
2019-12-12 18:14:42 +0200

    OD19DEVAT4-47 Created RemarkService. Include null checks and catch exceptions. Added Tests.
    Signed-off-by: Alex666 <08evalus07@gmail.com>

--- src/main/java/com/netcracker/testerritto/services/RemarkService.java
+++ src/main/java/com/netcracker/testerritto/services/RemarkService.java
@@ -1,47 +1,63 @@
 package com.netcracker.testerritto.services;
 
 import com.netcracker.testerritto.dao.RemarkDAO;
+import com.netcracker.testerritto.exceptions.ServiceException;
+import com.netcracker.testerritto.handlers.ServiceExceptionHandler;
+import com.netcracker.testerritto.models.Remark;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
+import org.springframework.transaction.TransactionException;
 
 import java.math.BigInteger;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 @Service
-@Transactional
 public class RemarkService {
 
     @Autowired
     private RemarkDAO remarkDAO;
 
-    private static Logger log=Logger.getLogger(RemarkService.class.getName());
+    @Autowired
+    private ServiceExceptionHandler serviceExceptionHandler;
 
-    public BigInteger createRemark(BigInteger userId, BigInteger questionId, String remarkText){
-        if(checkNotNull(userId) && checkNotNull(questionId) && !"".equals(remarkText))
-            return remarkDAO.createRemark(userId, questionId, remarkText);
-        else {
-            IllegalArgumentException exception = new IllegalArgumentException("Parametrs(userId, questionId, remarkText) can not be NULL.");
-            log.log(Level.SEVERE, exception.getMessage());
-            throw exception;
+    public Remark getRemarkById(BigInteger remarkId) throws ServiceException {
+        checkIdNotNull(remarkId);
+        try {
+            return remarkDAO.getRemarkById(remarkId);
+        } catch (DataAccessException exception) {
+            serviceExceptionHandler.logAndThrowServiceException("Failed GetRemarkById().", exception);
+            return null;
         }
     }
 
-    public void deleteRemark(BigInteger remarkId){
-        if(checkNotNull(remarkId))
+    public BigInteger createRemark(Remark remark) throws ServiceException {
+        checkIdNotNull(remark.getUserSenderId());
+        checkIdNotNull(remark.getQuestionId());
+        checkStringNotNull(remark.getText());
+        try {
+            return remarkDAO.createRemark(remark);
+        } catch (DataAccessException exception) {
+            serviceExceptionHandler.logAndThrowServiceException("Failed CreateRemark().", exception);
+            return null;
+        }
+    }
+
+    public void deleteRemark(BigInteger remarkId) throws ServiceException {
+        checkIdNotNull(remarkId);
+        try {
             remarkDAO.deleteRemark(remarkId);
-        else {
-            IllegalArgumentException exception = new IllegalArgumentException("Parametr RemarkID can not be NULL.");
-            log.log(Level.SEVERE, exception.getMessage());
-            throw exception;
+        } catch (DataAccessException exception) {
+            serviceExceptionHandler.logAndThrowServiceException("Failed DeleteRemark().", exception);
         }
     }
 
-    private boolean checkNotNull(BigInteger id){
-        if(id != null)
-            return true;
-        return false;
+    private void checkIdNotNull(BigInteger id) {
+        if (id == null)
+           serviceExceptionHandler.logAndThrowIllegalException("Parameter ID can not be NULL");
+    }
 
+    private void checkStringNotNull(String string) {
+        if ("".equals(string) || string == null)
+            serviceExceptionHandler.logAndThrowIllegalException("String parameter can not be NULL OR EMPTY.");
     }
 }
