46a28b5877e9d010aa81873fe64bc15e799ed885
Alex666 <08evalus07@gmail.com>
2019-12-16 20:11:43 +0200

    Reworked ServiceException. Extends from Runtime now. Added private final fields Throwable and message.
    Signed-off-by: Alex666 <08evalus07@gmail.com>

--- src/main/java/com/netcracker/testerritto/exceptions/ServiceException.java
+++ src/main/java/com/netcracker/testerritto/exceptions/ServiceException.java
@@ -1,7 +1,21 @@
 package com.netcracker.testerritto.exceptions;
 
-public class ServiceException extends Exception {
+
+public class ServiceException extends RuntimeException {
+    private final Throwable cause;
+    private final String message;
+
     public ServiceException(String message, Throwable cause) {
         super(message, cause);
+        this.cause = cause;
+        this.message = message;
+    }
+
+    public Throwable getCause() {
+        return cause;
+    }
+
+    public String getMessage() {
+        return message;
     }
 }
