942fadd991eb95bf288c0bfcd661568ff66d2fc3
Alex666 <08evalus07@gmail.com>
2019-12-16 20:10:02 +0200

    Added throwable and message to ApiRequestException. Removed throwable from ApiRequestHandler, added logger.
    Signed-off-by: Alex666 <08evalus07@gmail.com>

--- src/main/java/com/netcracker/testerritto/handlers/ApiExceptionHandler.java
+++ src/main/java/com/netcracker/testerritto/handlers/ApiExceptionHandler.java
@@ -2,6 +2,8 @@ package com.netcracker.testerritto.handlers;
 
 import com.netcracker.testerritto.exceptions.ApiException;
 import com.netcracker.testerritto.exceptions.ApiRequestException;
+import com.netcracker.testerritto.exceptions.ServiceException;
+import org.apache.log4j.Logger;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -12,16 +14,30 @@ import java.time.ZonedDateTime;
 
 @ControllerAdvice
 public class ApiExceptionHandler {
+    private static Logger logger = Logger.getLogger(ApiExceptionHandler.class.getName());
 
     @ExceptionHandler(value = {ApiRequestException.class})
-    public ResponseEntity<Object> handleApiRequestException(ApiRequestException e) {
+    public ResponseEntity<Object> handleServiceException(ApiRequestException e) {
+        logger.error(e.getMessage(), e);
         HttpStatus badRequest = HttpStatus.BAD_REQUEST;
 
         ApiException apiException = new ApiException(
-            e.getMessage(),
-            e,
-            badRequest,
-            ZonedDateTime.now(ZoneId.of("Z"))
+        e.getMessage(),
+        badRequest,
+        ZonedDateTime.now(ZoneId.of("Z"))
+        );
+        return new ResponseEntity<>(apiException, badRequest);
+    }
+
+
+    @ExceptionHandler(value = {ServiceException.class})
+    public ResponseEntity<Object> handleInternalError(ServiceException e) {
+        HttpStatus badRequest = HttpStatus.NOT_FOUND;
+
+        ApiException apiException = new ApiException(
+        e.getMessage(),
+        badRequest,
+        ZonedDateTime.now(ZoneId.of("Z"))
         );
         return new ResponseEntity<>(apiException, badRequest);
     }
