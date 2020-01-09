acd4b2dcd5026d7482b9fd1c8f841e3bb1894f75
Alex666 <08evalus07@gmail.com>
2020-01-09 19:04:52 +0200

    Updated group component. Added methods to show results, users ande remarks. Changed visual display. Changed result component. Display results for test now. Updated pass-test component. Add replies to DB now.
    Signed-off-by: Alex666 <08evalus07@gmail.com>

--- testeritto-front/src/app/group/group.component.ts
+++ testeritto-front/src/app/group/group.component.ts
@@ -6,6 +6,10 @@ import { UserService } from '../core/api/user.service';
 import { User } from '../core/models/user.model';
 import { GroupService } from '../core/api/group.service';
 import { Result } from '../core/models/result.model';
+import { ResultService } from '../core/api/result.service';
+import { DatePipe } from '@angular/common';
+import { PassTestService } from '../core/api/pass-test.service';
+import { Test } from '../core/models/test.model';
 
 @Component({
   selector: 'app-group',
@@ -15,46 +19,59 @@ import { Result } from '../core/models/result.model';
 export class GroupComponent implements OnInit {
   group: Group;
   user: User;
-  results: Array<Result>;
+  results: Array<Result> = new Array();
   finalResult: Map<Date,String>;
   loading: Boolean = false;
   testsLoaded: Boolean = false;
   showResult: Boolean = false;
   infoIfNoResult: String;
   selectedTest: BigInteger;
-
+  test: Test;
   constructor(private route: ActivatedRoute,
               private userService: UserService,
               private groupService: GroupService,
-              private router: Router) {
+              private resultService: ResultService,
+              private passTestService: PassTestService,
+              private router: Router,
+              private datePipe: DatePipe) {
     this.group = new Group;
     route.params.subscribe(params=>this.group.id=params['groupId']);  
    }
 
   ngOnInit() { 
+    var check;
     this.userService.getUser().subscribe((user: User) => {
       this.user = user; 
-      user.groups.forEach(element => {
-        if(element.id == this.group.id){
-          this.loading = true;
-          this.groupService.getUserResultsForTest(this.user.id).subscribe(data => {
-            this.user.results = data;
-          });
-          return;
-        }
-        this.router.navigate(['']);
-      });
-    });  
-    this.groupService.getGroup(this.group.id).subscribe((group: Group) => {
-      this.group = group;   
-      this.testsLoaded = true;
-    }); 
+      check = user.groups.find(element => element.id == this.group.id);   
+      if(check != undefined) {
+        this.loading = true;   
+        this.groupService.getGroup(this.group.id).subscribe((group: Group) => {
+          this.group = group;   
+          this.testsLoaded = true;
+          this.groupService.tests = this.group.tests;
+        }); 
+        this.groupService.getUserResultsForTest(this.user.id).subscribe(data => {
+          this.user.results = data;   
+        });
+      }         
+      else 
+        this.router.navigate(['/group']);     
+    });   
   }
 
-  seeResultsOnTest(id: BigInteger) {
+  showResultsOnTest(id: BigInteger) {
     this.selectedTest = id;
-    this.user.results.filter(element => element.testId = this.selectedTest)
-    this.showResult = true;
+    this.results = new Array();
+    var result = this.user.results.filter(element => element.testId == this.selectedTest)
+    result.forEach(element => {
+      if(this.results != undefined && this.results.find(elem => elem.status == element.status && elem.date == element.date) == undefined) {
+        this.results.push(element);
+      }
+       
+    })
+    if(this.results.length>0)
+      this.showResult = true;
+    else this.showResult = false;
   }
 
   copyLinkToClipboard() {
@@ -66,15 +83,39 @@ export class GroupComponent implements OnInit {
     document.execCommand('copy');
   }
 
-  // passTest(id: BigInteger) {
-  //   var now = new Date();
-  //   var nowDate = (now.getFullYear() + "-" + now.getMonth() + "-" + now.getDate()).toString();
-  //   var checkResult = this.user.results.find( element => 
-  //     element.date.toString() == nowDate.toString() && element.testId == id
-  //   )
-  //   if(checkResult == null)      
-  //     this.router.navigateByUrl('/pass-test/'+id);
-  //   else alert("You already passed test today. Come back tomorrow :)" + checkResult.id);
-  // }
+  passTest(id: BigInteger) {
+    var nowDate = this.datePipe.transform(new Date(),'yyyy-MM-dd');
+    var checkResult = this.user.results.find( element => element.date.toString() == nowDate && element.testId == id )
+    if(checkResult == undefined)      
+      this.router.navigateByUrl('/pass-test/' + this.user.id + '/test/' + id);
+    else alert("You already passed test today. Come back tomorrow :)");
+  }
 
+  finishTest(testId: BigInteger) {
+    this.passTestService.getFinishTest(this.user.id, testId).subscribe((test: Test) => {this.test = test; console.log(test.questions); });
+
+    this.router.navigate(['/pass-test', this.user.id, 'test', testId]);
+
+  }
+
+  showDetailsOnResult(date: Date) {
+    this.results = this.user.results.filter(element => element.testId == this.selectedTest 
+      && element.date == date && element.status == "PASSED")
+    this.resultService.results = this.results;
+    this.router.navigateByUrl('/group/'+this.group.id+'/results');
+  }
+
+  showUsersInGroup() {
+    this.groupService.getUsersInGroup(this.group.id).subscribe(data => {
+      this.groupService.users = data; 
+      this.router.navigateByUrl('/group/'+this.group.id+'/users');
+    });   
+  }
+
+  showAllRemarks() {
+    this.groupService.getAllRemarks(this.group.id).subscribe(data => {
+      this.groupService.remarks = data;
+      this.router.navigateByUrl('/group/'+this.group.id+'/remarks');
+    })    
+  }
 }
--- testeritto-front/src/app/result/result.component.ts
+++ testeritto-front/src/app/result/result.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Subscription } from 'rxjs';
 import { Result } from '../core/models/result.model';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { ResultService } from '../core/api/result.service';
 
 @Component({
@@ -10,18 +10,24 @@ import { ResultService } from '../core/api/result.service';
   styleUrls: ['./result.component.css']
 })
 export class ResultComponent implements OnInit {
-  result: Result;
-  private subscription: Subscription;
+  results: Array<Result>;
+  status: String;
+  date: Date;
+  groupId: BigInteger;
   constructor(private route: ActivatedRoute,
+              private router: Router,
               private resultService: ResultService) {
-    this.result = new Result();
-    this.subscription = route.params.subscribe(params=>this.result.id=params['resultId']);  
+    if(this.resultService.results == undefined){
+      route.params.subscribe(params=>this.groupId=params['groupId']);  
+      this.router.navigateByUrl('/group/'+this.groupId);
+    }
+    
    }
 
   ngOnInit() {
-    this.resultService.getResult(this.result.id).subscribe((result: Result) =>{
-      this.result = result;
-    })
+    this.results = this.resultService.results;
+    this.date = this.results.find(element => element.id!=undefined).date;
+    this.status = this.results.find(element => element.id!=undefined).status;
   }
 
 }
